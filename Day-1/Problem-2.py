68b7a034ca21737212813a1f63d0fc6849b3a612
Tarun Goel <ezCoder7@gmail.com>
2025-10-03 11:22:16 +0530

    1 and 2

---
+++ Day-1/Problem-2.py
@@ -0,0 +1,7 @@
+# Area of a Circle
+
+import math
+class Solution:
+  def calculateArea(self,r):
+    pie = math.pi
+    return pie*r*r
---
+++ Day-1/Problem-3.py
@@ -0,0 +1,12 @@
+# Leap Year
+
+class Solution:
+    def checkYear (self, n):
+        if n%100!=0 and n%4==0:
+            return True
+        elif n%100!=0 and n%4!=0:
+            return False
+        elif n%400==0:
+            return True
+        else:
+            return False
